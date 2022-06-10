28bf389b01b019e1be78f18d4fbead06bab203ac
Kevin Paul <kpaul@ucar.edu>
2022-06-10 16:55:38 -0600

    Always set the port

--- sshspawner/sshspawner.py
+++ sshspawner/sshspawner.py
@@ -173,9 +173,14 @@ class SSHSpawner(Spawner):
             for index, value in enumerate(cmd):
                 if value == old:
                     cmd[index] = new
+
+        has_port = False
         for index, value in enumerate(cmd):
             if value[0:6] == "--port":
                 cmd[index] = "--port=%d" % (port)
+                has_port = True
+        if not has_port:
+            cmd.append(f"--port={port}")
 
         remote_cmd = " ".join(cmd)
 
